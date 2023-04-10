4673336110b9c2b872264d1cf3a0c68b359ad294
Moo-Young Chun <honu1211@gmail.com>
2023-04-10 09:56:04 -0400

    Add files via upload
    sitcomtn-052

---
+++ conf.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#
+# Sphinx configuration file
+# see metadata.yaml in this repo to update document-specific metadata
+
+from documenteer.conf.technote import * # noqa: F401, F403
